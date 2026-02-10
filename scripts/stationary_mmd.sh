75e8c7b0ef89a401f80a6ba52299c19fc9548805
hudsonchen <hudsonchen33@outlook.com>
2026-02-10 06:12:47 +0000

    update_configs

--- scripts/stationary_mmd.sh
+++ scripts/stationary_mmd.sh
@@ -1,6 +1,5 @@
 ## mixture of Gaussians
 particle_num=10
-
 python main.py --seed 0 --particle_num $particle_num --dataset mog --step_size 1.0 --bandwidth 1.0 --inject_noise_scale 0.0 --step_num 10000
 
 for particle_num in 30 100
@@ -37,14 +36,14 @@ for seed in {0}
 do
   for particle_num in 10 30 100
   do
-    /home/zongchen/miniconda3/envs/mmd_cubature/bin/python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
+    python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
   done
   for particle_num in 300
   do
-    /home/zongchen/miniconda3/envs/mmd_cubature/bin/python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
+    python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 1.0 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
   done
   for particle_num in 1000
   do
-    /home/zongchen/miniconda3/envs/mmd_cubature/bin/python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 0.3 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
+    python main.py --seed $seed --particle_num $particle_num --dataset house_8L --step_size 0.3 --bandwidth 1.0 --inject_noise_scale 1.0 --step_num 100000
   done
 done
